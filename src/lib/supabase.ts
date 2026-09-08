52c48c3c6195e818ea96e319b2f308bb57875911
hushpark <parkdemon@gmail.com>
2026-09-08 16:24:25 +0900

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // 🎯 API Keys 메뉴에서 복사한 주소와 anon key를 여기에 직접 넣어주세요.
-const supabaseUrl = 'https://hatiediksxugptftfalc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhdGllZGlrc3h1Z3B0ZnRmYWxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NTQ1OTUsImV4cCI6MjEwNDEzMDU5NX0.Hb9D-GpjhBSb5zIVC-pHpf3Y36khSrWgO7vJOjFN09c';
+const supabaseUrl = 'https://fmboamnuoajpishhqrty.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtYm9hbW51b2FqcGlzaGhxcnR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2NzA2MDYsImV4cCI6MjEwMjI0NjYwNn0.qGO0BsiAzHUnd4Z8gmffxoB-ZIMc26XAlhYgBVwCz7s';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
